fa74c2ab9a3dc1abbf3d39acc69f7e0a4d2ccb53
Quentin Maia <q.maia@laposte.net>
2025-10-03 16:47:41 +0200

    Add files via upload

---
+++ Villes/ExoRequete.sql
@@ -0,0 +1,98 @@
+/*
+Exercice Requêtes SQL VILLES
+
+                    Exercice 1
+
+Obtenir la liste des 10 villes les plus peuplées en 2012
+
+
+
+
+
+
+**********************************************************************************************************
+                    Exercice 2
+
+Obtenir la liste des 50 villes ayant la plus faible superficie
+
+
+
+
+**********************************************************************************************************
+                    Exercice 3
+
+Obtenir la liste des départements d’outres-mer, c’est-à-dire ceux dont le numéro de département commencent par “97”
+
+
+
+
+
+**********************************************************************************************************
+                    Exercice 4
+
+Obtenir le nom des 10 villes les plus peuplées en 2012, ainsi que le nom du département associé
+
+
+
+
+**********************************************************************************************************
+                    Exercice 5
+
+Obtenir la liste du nom de chaque département, associé à son code et du nombre de commune au sein de ces département,
+en triant afin d’obtenir en priorité les départements qui possèdent le plus de communes
+
+
+
+
+**********************************************************************************************************
+                    Exercice 6
+
+Obtenir la liste des 10 plus grands départements, en terme de superficie
+
+
+
+
+**********************************************************************************************************
+                    Exercice 7
+
+Compter le nombre de villes dont le nom commence par “Saint”
+
+
+
+
+**********************************************************************************************************
+                    Exercice 8
+
+Obtenir la liste des villes qui ont un nom existants plusieurs fois, 
+et trier afin d’obtenir en premier celles dont le nom est le plus souvent utilisé par plusieurs communes
+
+
+
+
+**********************************************************************************************************
+                    Exercice 9
+
+Obtenir en une seule requête SQL la liste des villes dont la superficie est supérieur à la superficie moyenne
+
+
+
+
+**********************************************************************************************************
+                    Exercice 10
+
+Obtenir la liste des départements qui possèdent plus de 2 millions d’habitants
+
+
+
+
+
+**********************************************************************************************************
+                    Exercice 11
+
+Remplacez les tirets par un espace vide, pour toutes les villes commençant par “SAINT-” 
+(dans la colonne qui contient les noms en majuscule)
+
+
+
+
+**********************************************************************************************************
